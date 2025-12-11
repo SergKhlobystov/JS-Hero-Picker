f3225b67ff18e554173ad30b6347e5720c6ef418
SergKhlobystov <khlobystov.serg@gmail.com>
2025-12-11 17:14:34 +0200

    heroes 1st database

---
+++ data.js
@@ -0,0 +1,9 @@
+export const heroesData = [
+    {
+        name: ["Alice Lightbringer"],
+        class: ["Holly Sorcerer"],
+        image: "holly.jpg",
+        video: "holly.mp4",
+        isEvil: false,
+    }
+]
